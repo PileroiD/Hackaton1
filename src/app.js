cb0c02229df71720eeb4b8c6e7c45038d1b4baea
Alexandr Lozynskyi <pileroid@gmail.com>
2023-10-13 21:19:57 -0600

    Feature of opening a custom context menu was added, html file was changed with adding some li, styles was changed with width auto for context menu

--- src/app.js
+++ src/app.js
@@ -1,2 +1,15 @@
+import { ContextMenu } from './menu';
 import './styles.css'
 
+document.addEventListener('DOMContentLoaded', () => {
+    const mainScope = document.querySelector('body'),
+        contextMenu = new ContextMenu('#menu');
+
+    mainScope.addEventListener('contextmenu', event => {
+        event.preventDefault();
+        const {target} = event;
+        if (target) {
+            contextMenu.open(event);
+        }
+    });
+});
--- src/menu.js
+++ src/menu.js
@@ -1,5 +1,23 @@
 import {Menu} from './core/menu'
 
 export class ContextMenu extends Menu {
+    constructor(selector) {
+        super(selector);
+    }
 
+    open(event) {
+        const {clientX: cX, clientY: cY} = event;
+
+        this.el.style.top = `${cY}px`;
+        this.el.style.left = `${cX}px`;
+
+        this.el.onClick = () => this.close();
+        this.el.classList.add('open');
+    }
+
+    close() {
+        this.el.classList.remove('open');
+    }
+
+    add() {}
 }
