f1df368754863dff650ec4927654c26f54627c59
doograssen <rythmag@gmail.com>
2023-10-15 02:55:38 +0300

    add shape module feature

--- src/app.js
+++ src/app.js
@@ -1,18 +1,19 @@
 import { Timer } from './modules/timer.module';
 import { ClicksModule } from './modules/clicks.module';
+import { ShapeModule } from './modules/shape.module';
 import { ContextMenu } from './menu';
 import './styles.css';
 
 document.addEventListener('DOMContentLoaded', () => {
   const mainScope = document.querySelector('body'),
-    contextMenuSelector = new ContextMenu('#menu'),
+    contextMenuItem = new ContextMenu('#menu'),
     menu = document.querySelector('#menu');
 
   mainScope.addEventListener('contextmenu', (event) => {
     event.preventDefault();
     const { target } = event;
     if (target) {
-      contextMenuSelector.open(event);
+      contextMenuItem.open(event);
     }
   });
 
@@ -23,6 +24,9 @@ document.addEventListener('DOMContentLoaded', () => {
   const timer = new Timer('timer', 'Timer');
   menu.append(timer.toHTML());
 
+  const shape = new ShapeModule('shape', 'Shape');
+  menu.append(shape.toHTML());
+
   menu.addEventListener('click', (event) => {
     if (event && event.target.classList.contains('menu-item')) {
       switch (event.target.dataset.type) {
@@ -31,6 +35,12 @@ document.addEventListener('DOMContentLoaded', () => {
           break;
         case 'timer':
           timer.trigger(5);
+          break;
+        case 'shape':
+          const targetParent = event.target.offsetParent;
+          shape.nullPoint.x = event.target.offsetParent.getBoundingClientRect().x;
+          shape.nullPoint.y = event.target.offsetParent.getBoundingClientRect().y;
+          shape.trigger();
           break;
       }
     }
