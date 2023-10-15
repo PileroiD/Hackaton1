b5b30000719bcbccabe2c59c43a893e3d6d1eda5
Irina Borisova <i.a.borisova@gmail.com>
2023-10-15 15:50:41 +0200

    Ran formatter prettier

--- src/app.js
+++ src/app.js
@@ -1,100 +1,110 @@
-import { Timer } from './modules/timer.module';
-import { ClicksModule } from './modules/clicks.module';
-import { RandomMassage } from  './modules/message.module';
-import { RandomSound } from './modules/sound.module';
-import { ShapeModule } from './modules/shape.module';
+import { Timer } from "./modules/timer.module";
+import { ClicksModule } from "./modules/clicks.module";
+import { RandomMassage } from "./modules/message.module";
+import { RandomSound } from "./modules/sound.module";
+import { ShapeModule } from "./modules/shape.module";
 
-import { ContextMenu } from './menu';
-import { BackgroundModule } from './modules/background.module'; // фон-картинка
-import { DevelopersInfoModule } from './modules/developersInfo.module';
-import './styles.css';
-import { showForm, checkNumInputs, closeForm, clearInputs, showErrorForm } from './utils';
+import { ContextMenu } from "./menu";
+import { BackgroundModule } from "./modules/background.module"; // фон-картинка
+import { DevelopersInfoModule } from "./modules/developersInfo.module";
+import "./styles.css";
+import {
+  showForm,
+  checkNumInputs,
+  closeForm,
+  clearInputs,
+  showErrorForm,
+} from "./utils";
 
-document.addEventListener('DOMContentLoaded', () => {
-  const mainScope = document.querySelector('body'),
-    contextMenuItem = new ContextMenu('#menu'),
-        menu = document.querySelector('#menu');
+document.addEventListener("DOMContentLoaded", () => {
+  const mainScope = document.querySelector("body"),
+    contextMenuItem = new ContextMenu("#menu"),
+    menu = document.querySelector("#menu");
 
-  mainScope.addEventListener('contextmenu', (event) => {
-        event.preventDefault();
-        const {target} = event;
-        if (target) {
+  mainScope.addEventListener("contextmenu", (event) => {
+    event.preventDefault();
+    const { target } = event;
+    if (target) {
       contextMenuItem.open(event);
-        }
-    });
+    }
+  });
 
+  const clickFeature = new ClicksModule("click", "Click analytics");
+  menu.append(clickFeature.toHTML());
 
-    const clickFeature = new ClicksModule('click', 'Click analytics');
-    menu.append(clickFeature.toHTML());
+  const timer = new Timer("timer", "Timer");
+  menu.append(timer.toHTML());
 
-    const timer = new Timer('timer', 'Timer');
-    menu.append(timer.toHTML());
+  const shape = new ShapeModule("shape", "Shape");
+  menu.append(shape.toHTML());
 
-    const shape = new ShapeModule('shape', 'Shape');
-    menu.append(shape.toHTML());
+  // фон-картинка
+  const changeImage = new BackgroundModule("Change Style", "Change Style");
+  menu.append(changeImage.toHTML());
 
-   // фон-картинка
-    const changeImage = new BackgroundModule('Change Style', 'Change Style');
-    menu.append(changeImage.toHTML());
+  const randomMessage = new RandomMassage("message", "Random message");
+  menu.append(randomMessage.toHTML());
 
-    const randomMessage = new RandomMassage('message', 'Random message');
-    menu.append(randomMessage.toHTML());
+  const randomSound = new RandomSound("sound", "Random sound");
+  menu.append(randomSound.toHTML());
 
-    const randomSound = new RandomSound('sound', 'Random sound')
-    menu.append(randomSound.toHTML());
+  // module about developers
+  const developersInfo = new DevelopersInfoModule(
+    "developersInfo",
+    "Developers"
+  );
+  menu.append(developersInfo.toHTML());
 
- // module about developers
-    const developersInfo = new DevelopersInfoModule('developersInfo', 'Developers');
-    menu.append(developersInfo.toHTML());
-
-  menu.addEventListener('click', (event) => {
-        if (event && event.target.classList.contains('menu-item')) {
-            switch (event.target.dataset.type) {
-                case 'click':
+  menu.addEventListener("click", (event) => {
+    if (event && event.target.classList.contains("menu-item")) {
+      switch (event.target.dataset.type) {
+        case "click":
           clickFeature.trigger(3000, 1000);
-                    break;
-                case 'timer':
+          break;
+        case "timer":
           const contextMenu = new ContextMenu("#menu");
           contextMenu.close();
-          
-          showForm({text: "Укажите количество секунд для таймера"});
-          checkNumInputs('input');
 
-          const form = document.querySelector('form'),
-                input = form.querySelector('input'),
-                button = form.querySelector('button');
+          showForm({ text: "Укажите количество секунд для таймера" });
+          checkNumInputs("input");
 
-          form.addEventListener('submit', event => {
+          const form = document.querySelector("form"),
+            input = form.querySelector("input"),
+            button = form.querySelector("button");
+
+          form.addEventListener("submit", (event) => {
             event.preventDefault();
 
             if (input.value && +input.value <= 60) {
               closeForm();
               timer.trigger(+input.value);
-              clearInputs('input');
+              clearInputs("input");
             } else {
-              showErrorForm({form, input, button});
+              showErrorForm({ form, input, button });
             }
           });
-                    break;
-                 case 'Change Style':
-                    changeImage.trigger(); // фон-картинка
-                    break;
-                case 'message':
-                    randomMessage.trigger()
-                    break;
-                case 'sound':
-                    randomSound.trigger()
-                    break;
-                case 'shape':
-          	    const targetParent = event.target.offsetParent;
-          	    shape.nullPoint.x = event.target.offsetParent.getBoundingClientRect().x;
-          	    shape.nullPoint.y = event.target.offsetParent.getBoundingClientRect().y;
-          	    shape.trigger();
-          	    break;
-        	case 'developersInfo':
-          	    developersInfo.trigger();
-          	    break;
-            }
-        }
-    });
+          break;
+        case "Change Style":
+          changeImage.trigger(); // фон-картинка
+          break;
+        case "message":
+          randomMessage.trigger();
+          break;
+        case "sound":
+          randomSound.trigger();
+          break;
+        case "shape":
+          const targetParent = event.target.offsetParent;
+          shape.nullPoint.x =
+            event.target.offsetParent.getBoundingClientRect().x;
+          shape.nullPoint.y =
+            event.target.offsetParent.getBoundingClientRect().y;
+          shape.trigger();
+          break;
+        case "developersInfo":
+          developersInfo.trigger();
+          break;
+      }
+    }
+  });
 });
