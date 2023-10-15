342f19c126a6f3dfa0c52f99e7cd5b4c52aef63f
Irina Borisova <i.a.borisova@gmail.com>
2023-10-15 22:12:57 +0200

    refactoring

--- src/modules/message.module.js
+++ src/modules/message.module.js
@@ -12,15 +12,15 @@ export class RandomMassage extends Module {
         'А в какой ветке ты делаешь?', 
         'Смёрджил в девелоп', 
         'Надо немного покумекать', 
-        "Я замержила в develop", 
-        "Давай тогда новую ветку делать",
+        'Я замержила в develop', 
+        'Давай тогда новую ветку делать',
         'А в девелоп всё пушится нормально?',
         'Пуш конечно',
         'Мержу?'
     ];
 
     const divMessage = document.createElement('div');
-    divMessage.classList.add("divTimer");
+    divMessage.classList.add('divTimer');
     let randomIndex = random(0, 7);
     divMessage.textContent = messageList[randomIndex];
     document.body.append(divMessage);
