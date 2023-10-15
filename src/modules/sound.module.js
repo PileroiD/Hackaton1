4be49119c40f8dbbf1664bc66b0f1795546ef2d9
Irina Borisova <i.a.borisova@gmail.com>
2023-10-15 16:35:21 +0200

    aded several sounds

--- src/modules/sound.module.js
+++ src/modules/sound.module.js
@@ -13,9 +13,17 @@ export class RandomSound extends Module {
         const contextMenu = new ContextMenu("#menu")
         contextMenu.close()
 
-        let randomIndex = random(0, 2)
+        let randomIndex = random(0, 7)
 
-        let audios = ["https://samplelib.com/lib/preview/wav/sample-3s.wav", "http://soundbible.com/grab.php?id=1778&type=mp3", "http://soundbible.com/grab.php?id=1949&type=mp3"]
+        let audios = [
+            "https://samplelib.com/lib/preview/wav/sample-3s.wav", 
+            "http://soundbible.com/grab.php?id=1778&type=mp3", 
+            "http://soundbible.com/grab.php?id=1949&type=mp3", 
+            "http://soundbible.com/grab.php?id=96&type=mp3", 
+            "http://soundbible.com/grab.php?id=1312&type=mp3", 
+            "http://soundbible.com/grab.php?id=722&type=mp3", 
+            "http://soundbible.com/grab.php?id=1320&type=mp3",
+            "http://soundbible.com/grab.php?id=2026&type=mp3"]
 
         let audio = new Audio(audios[randomIndex])
         audio.play()
